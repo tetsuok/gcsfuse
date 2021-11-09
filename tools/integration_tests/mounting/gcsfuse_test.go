91999e24aa4fab7da69004bb94e149fe424e6849
Tetsuo Kiso <kiso@hey.com>
2021-11-09 15:17:02 +0900

    Revert "Fix NonEmptyMountPoint"
    This reverts commit bc766f3cb517f40b1f1dc77354a11c1481eefea7.

--- tools/integration_tests/mounting/gcsfuse_test.go
+++ tools/integration_tests/mounting/gcsfuse_test.go
@@ -148,7 +148,7 @@ func (t *GcsfuseTest) NonEmptyMountPoint() {
 
 	// Write a file into the mount point.
 	p := path.Join(t.dir, "foo")
-	err = ioutil.WriteFile(p, []byte("hello"), 0600)
+	err = ioutil.WriteFile(p, nil, 0600)
 	AssertEq(nil, err)
 
 	defer os.Remove(p)
@@ -157,7 +157,7 @@ func (t *GcsfuseTest) NonEmptyMountPoint() {
 	args := []string{canned.FakeBucketName, t.dir}
 
 	err = t.runGcsfuse(args)
-	AssertNe(nil, err)
+	ExpectThat(err, Error(HasSubstr("is not empty")))
 }
 
 func (t *GcsfuseTest) MountPointIsAFile() {
